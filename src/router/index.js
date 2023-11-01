a97833464163e2aef76ec19c26fca9bc841b7a9b
Tr-Vinh <nguyenphatdat@HaiTrieuLedScreens-MacBook-Pro-4.local>
2023-11-01 07:44:32 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,14 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
-}, ];
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+
+    },
+];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
